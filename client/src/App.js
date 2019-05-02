85dbb10eb8722d826b7eea5767fd3b7d9df396ab
ivancho277 <rouskoi@live.com>
2019-05-01 18:56:48 -0700

    added Navbar component and About card

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from "./pages/home";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import AboutCard from "./components/AboutCard"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Navbar />
+          <AboutCard />
       </div>
     );
   }
---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+function home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default home
+
