3fe1ab604c3fee5429006456b98239d873e0e36b
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-05-02 14:57:44 -0700

    jumbotrons working

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import AboutCard from "../components/AboutCard";
 import CharacterCard from '../components/CharacterCard'
+import Jumbotron1 from '../components/Jumbotron1'
+import Jumbotron2 from '../components/Jumbotron2'
 
 function Home() {
-  let image = 'https://i2.wp.com/military-fail.com/wp-content/uploads/2015/04/crazy-ivan.jpg?resize=500%2C395'
-  return (
+  let image = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3jNvW0v3hAhW2CjQIHa26Cv8QjRx6BAgBEAU&url=http%3A%2F%2Fmilitary-fail.com%2Fcrazy-ivan%2F&psig=AOvVaw1xBjGk0ZZJulH05poMYguL&ust=1556913966451867"
+    return (
     <div>
-      <CharacterCard src= {image}/>
+      <CharacterCard src = 'image'/>
+      <Jumbotron1/>
+      
+      <Jumbotron2/>
     </div>
   )
 }
