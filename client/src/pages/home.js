c1dd97ea20cca42e2854892f14f4b27ce3a53514
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-05-02 13:24:17 -0700

    character card crazy ivan done

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import AboutCard from "../components/AboutCard";
+import CharacterCard from '../components/CharacterCard'
 
 function Home() {
+  let image = 'https://i2.wp.com/military-fail.com/wp-content/uploads/2015/04/crazy-ivan.jpg?resize=500%2C395'
   return (
     <div>
-      <AboutCard />
+      <CharacterCard src= {image}/>
     </div>
   )
 }
