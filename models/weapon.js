3f760682b7ae88ebea58fb80d3c00a7f96094d6a
Bronson <bt98199@gmail.com>
2019-05-01 20:34:39 -0700

    EOD 1 may 2019

--- models/weapon.js
+++ models/weapon.js
@@ -5,7 +5,9 @@ const weaponSchema = new Schema({
   weapon: { type: String, required: true },
   damage: { type: Array, required: false }, // weapon damage is a die roll, but can easily be created as an array of numbers
   min_strength: { type: Number, required: true }, // higher the strength, stronger the weapon
-  type: { type: String, required: true }, // choices are melee (1 hex away), pole (can jab 2 hex away), and missile
+  type: { type: String, required: true }, // choices are melee (1 hex away), pole (can jab 2 hex away), ranged, and beast
+  throwable: { type: boolean, required: false, default: false }, // Can the item be thrown?
+  twohands: { type: boolean, required: false, default: false }, // Does the item require two hands? (thus, no shield usable)
   notes: { type: String, required: false }
 });
 const Weapon = mongoose.model("Weapon", weaponSchema);
