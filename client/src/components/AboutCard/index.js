6f3aa5bc27b803d2b2e9c7e1c71d36f8a6971b32
ivan rouskov <rouskoi@live.com>
2019-05-02 12:48:33 -0700

    Ivan react paths (#9)
    * added Navbar component and About card
    
    * created pages, as well as about card and home page

---
+++ client/src/components/AboutCard/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AboutCard() {
+  return (
+    <div>
+      <div class="card text-left">
+        <div class="card-body">
+          <h4 class="card-title">About the Melee!</h4>
+          <p class="card-text">
+            I'm beginning to feel like a Rap God, Rap God. All my people from
+            the front to the back nod, back nod. Now who thinks their arms are
+            long enough to slap box, slap box?. Let me show you maintaining this
+            shit ain't that hard, that hard. Everybody want the key and the
+            secret. To rap immortality like I have got.I'm Slim Shady, yes, I'm
+            the real Shady. All you other Slim Shadys are just imitating. So
+            won't the real Slim Shady please stand up. Please stand up, please
+            stand up?. 'Cause I'm Slim Shady, yes, I'm the real Shady. All you
+            other Slim Shadys are just imitating. So won't the real Slim Shady
+            please stand up. Please stand up, please stand up?My tea's gone
+            cold. I'm wondering why I got out of bed at all. The morning rain
+            clouds up my window. And I can't see at all. And even if I could
+            it'd all be gray. But your picture on my wall. It reminds me that
+            it's not so bad, it's not so bad.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutCard;
---
+++ client/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import AboutCard from "../components/AboutCard";
+
+function Home() {
+  return (
+    <div>
+      <AboutCard />
+    </div>
+  )
+}
+
+export default Home
+
