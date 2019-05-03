3038f0a90c60004b52c4a8717495c396742ce1e0
ivan rouskov <rouskoi@live.com>
2019-05-03 14:07:39 -0700

    first round of paths working (#27)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,32 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import AboutCard from "../components/AboutCard";
-import CharacterCard from '../components/CharacterCard'
-import Jumbotron1 from '../components/Jumbotron1'
-import Jumbotron2 from '../components/Jumbotron2'
-import { Col, Row, Container } from '../components/Grid'
+import CharacterCard from '../components/CharacterCard';
+import Jumbotron1 from '../components/Jumbotron1';
+import Jumbotron2 from '../components/Jumbotron2';
+import { Col, Row, Container } from "../components/Grid";
 
 function Home() {
-  let stats = {
-    strength: 11,
-    dexterity: 13
-  }
-  let src = "https://placeimg.com/640/480/people"
+
   return (
     <div>
-      <Container>
-        <Row>
-          <Col size='4'>
-            <Jumbotron1 />
-          </Col>
-          <Col size='4'>
-          <CharacterCard stats={stats} src={src} />
-          </Col>
-          <Col size='4'>
-            <Jumbotron2 />
-          </Col>
-        </Row>
-      </Container>
+          <AboutCard />
+        
     </div>
   );
 }
