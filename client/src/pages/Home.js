92c43fa2a48c96af605bea25c923f66f17ed2ab2
Jordan Babbitt <JBabbitt27@gmail.com>
2019-05-03 17:52:42 -0700

    dice addition

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,18 +4,23 @@ import AboutCard from "../components/AboutCard";
 import CharacterCard from '../components/CharacterCard'
 import Jumbotron1 from '../components/Jumbotron1'
 import Jumbotron2 from '../components/Jumbotron2'
+import DiceComponent from '../components/DiceRoller'
 
 function Home() {
   let stats = {
     strength: 11,
     dexterity: 13
   }
+  let numDice = 3
 let src = "https://placeimg.com/640/480/people" 
    return (
     <div> 
       <Jumbotron1/>
       <CharacterCard stats={stats} src={src} />
       <Jumbotron2/>
+      <DiceComponent
+      numDice={numDice}
+      />
     </div>
   );
 }
