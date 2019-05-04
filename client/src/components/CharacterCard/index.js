b044a62b2ecf300fc843127545dfb7190c73648f
Anthonyme0328 <46856315+Anthonyme0328@users.noreply.github.com>
2019-05-03 17:13:58 -0700

    Card buttons (#33)
    * cards render but no image yet
    
    * img working on premade char card
    
    * names now working
    
    * changes to sizing of jumbo
    
    * right side set up with hard code
    
    * select buttons added to cards

--- client/src/components/CharacterCard/index.js
+++ client/src/components/CharacterCard/index.js
@@ -5,16 +5,17 @@ function  CharacterCard(props){
   return (
 <div>
   <div className="card" style={{width: 22 + 'rem'}}>
-    <h3 className= 'card-text'>Name: {props.name}</h3>
+    <h3 className= 'card-text'>{props.name}</h3>
     <img className="card-img-top" src={ props.src } alt="Card cap"/>
      <div className="card-body">
      <div  className= 'card'><u>Stats</u>
      Strength: {props.stats.strength}<br></br>
-     Dexterity: {props.stats.dexterity}<br></br>
+     Dexterity: {props.stats.strength}<br></br>
      HP: {props.stats.strength}
      </div>
       <p className="card-text">Is Cold and Dangerous in Mother Russia</p>
      </div>
+     <button type="button" class="btn btn-warning">Select</button>
   </div>
 </div>
   )
