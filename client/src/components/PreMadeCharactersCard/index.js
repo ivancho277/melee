6766dc88fe4ed95a3b9d7b964c6d2eb83b3610f8
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-05-07 14:56:31 -0700

    update

--- client/src/components/PreMadeCharactersCard/index.js
+++ client/src/components/PreMadeCharactersCard/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './style.css'
 
-<<<<<<< HEAD
 function PreMadeChar(props) {
-=======
-
-export function PreMadeChar1(props) {
->>>>>>> 3661126e31d3c35c40ca43d663b37b8265ddb74f
   return (
     <div>
       <div className='card' style={{width: 18 + 'rem'}} >
@@ -26,68 +21,4 @@ export function PreMadeChar1(props) {
   )
 }
 
-<<<<<<< HEAD
 export default PreMadeChar;
-=======
-
-export function PreMadeChar2(props) {
-  return (
-    <div>
-      <div className='card'>
-        <h3 className='card-text'>{props.name}</h3>
-        <img className='card-img-top' src={props.src} />
-        <div className='card-body'>
-          <div className='card'><u>Stats</u>
-            Strength:{props.stats.strength} <br></br>
-            Dexterity: {props.stats.strength}<br></br>
-            HP: {props.stats.strength}
-          </div>
-          <p className='card-text'>words words words</p>
-        </div>
-        <button type="button" class="btn btn-warning">Select</button>
-      </div>
-    </div>
-  )
-}
-
-export function PreMadeChar3(props) {
-  return (
-    <div>
-      <div className='card'>
-        <h3 className='card-text'>{props.name}</h3>
-        <img className='card-img-top' src={props.src} />
-        <div className='card-body'>
-          <div className='card'><u>Stats</u>
-            Strength:{props.stats.strength} <br></br>
-            Dexterity: {props.stats.strength}<br></br>
-            HP: {props.stats.strength}
-          </div>
-          <p className='card-text'>words words words</p>
-        </div>
-        <button type="button" class="btn btn-warning">Select</button>
-      </div>
-    </div>
-  )
-}
-
-
-export function PreMadeChar4(props) {
-  return (
-    <div>
-      <div className='card'>
-        <h3 className='card-text'>{props.name}</h3>
-        <img className='card-img-top' src={props.src} />
-        <div className='card-body'>
-          <div className='card'><u>Stats</u>
-            Strength:{props.stats.strength} <br></br>
-            Dexterity: {props.stats.strength}<br></br>
-            HP: {props.stats.strength}
-          </div>
-          <p className='card-text'>words words words</p>
-        </div>
-        <button type="button" class="btn btn-warning">Select</button>
-      </div>
-    </div>
-  )
-}
->>>>>>> 3661126e31d3c35c40ca43d663b37b8265ddb74f
