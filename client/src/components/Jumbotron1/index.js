283d8de99f3f5bf26b1829b2baca94c043dd703d
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-05-07 17:47:51 -0700

    cards match schema info

--- client/src/components/Jumbotron1/index.js
+++ client/src/components/Jumbotron1/index.js
@@ -4,42 +4,64 @@ import PreMadeChar from "../PreMadeCharactersCard"
 
 
 function Jumbotron1() {
-    let name = 'Crazy Ivan'
-    let name1= 'bob'
-    let name2= 'kevin'
-    let name3= 'Chris P. Bacon'
 
-      let stats = {
-        strength: 11,
-        dexterity: 13
-      } 
       let stats1 = {
-        strength: 12,
-        dexterity: 12
+        strength: 11,
+        dexterity: 13,
+        name: 'Crazy Ivan',
+        src: 'https://placeimg.com/641/481/people',
+        movementAllowance: 10,
+        weapon: "",
+        armor: "",
+        sheild: "",
+        life_status: 1
       } 
       let stats2 = {
-        strength: 13,
-        dexterity: 11
-      }
-      let stats3 = {
-        strength: 14,
-        dexterity: 10
+        strength: 12,
+        dexterity: 12,
+        name: 'Jordapeno',
+        src: 'https://placeimg.com/640/480/people',
+        movementAllowance: 10,
+        weapon: "",
+        armor: "",
+        sheild: "",
+        life_status: 1
       } 
-      let src = 'https://placeimg.com/641/481/people'
-      let src1 = 'https://placeimg.com/640/480/people'
-      let src2 = 'https://placeimg.com/642/482/people'
-      let src3 = 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjX-sfhsYriAhUFvZ4KHdyICK8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fscience-environment-43928318&psig=AOvVaw0njEheShFm7OS__61R7uk9&ust=1557351815519066'
+      let stats3 = {
+        strength: 13,
+        dexterity: 11,
+        name: 'Turnson Bronquist',
+        src: 'https://placeimg.com/642/482/people',
+        movementAllowance: 10,
+        weapon: "",
+        armor: "",
+        sheild: "",
+        life_status: 1
+      }
+      let stats4 = {
+        strength: 14,
+        dexterity: 10,
+        name: 'Chris P. Bacon',
+        src: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjX-sfhsYriAhUFvZ4KHdyICK8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fscience-environment-43928318&psig=AOvVaw0njEheShFm7OS__61R7uk9&ust=1557351815519066',
+        movementAllowance: 10,
+        weapon: "",
+        armor: "",
+        sheild: "",
+        life_status: 1
+      } 
+
+      
   return (
     <div>
       <div id="j1" className="jumbotron col-12" >
         <div>
-          <PreMadeChar stats={stats} src={src} name={name}/>
+          <PreMadeChar stats={stats1}/>
           <br></br>
-          <PreMadeChar stats={stats1} src={src1} name={name1}/>
+          <PreMadeChar stats={stats2}/>
           <br></br>
-          <PreMadeChar stats={stats2} src={src2} name={name2}/>
+          <PreMadeChar stats={stats3}/>
           <br></br>
-          <PreMadeChar stats={stats3} src={src3} name={name3}/>
+          <PreMadeChar stats={stats4}/>
           <br></br>
         </div>
       </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,18 +6,14 @@ import Jumbotron1 from '../components/Jumbotron1'
 import Jumbotron2 from '../components/Jumbotron2'
 import DiceComponent from '../components/DiceRoller'
 
+
 function Home() {
-  let stats = {
-    strength: 11,
-    dexterity: 13
-  }
+  
   let numDice = 3
 let src = "https://placeimg.com/640/480/people" 
    return (
     <div> 
-      {/* <Jumbotron1/> */}
       <AboutCard />
-      {/* <Jumbotron2/> */}
       <DiceComponent
       numDice={numDice}
       />
