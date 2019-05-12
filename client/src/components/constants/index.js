3dab5e826a7ae95675a163168be85cbf2b947f92
Bronson <bt98199@gmail.com>
2019-05-12 14:08:19 -0700

    data on combat section

---
+++ client/src/components/constants/index.js
@@ -0,0 +1,58 @@
+export const initialCharacters = [
+  {
+    id: 1,
+    name: "Ragnar the Impetuous",
+    hp: 11,
+    strength: 11,
+    dexterity: 13,
+    adjDx: 10,
+    weapon: "Short Sword",
+    wDmg: "2d6-1",
+    armor: "Leather Armor",
+    shield: "Large Shield",
+    armorAbsorbs: 4,
+    adjMovement: 8
+  },
+  {
+    id: 2,
+    name: "WUF!",
+    hp: 10,
+    strength: 10,
+    dexterity: 14,
+    adjDx: 14,
+    weapon: "Teeph!",
+    wDmg: "1d6+1",
+    armor: "Pelt",
+    shield: "",
+    armorAbsorbs: 1,
+    adjMovement: 12
+  },
+  {
+    id: 3,
+    name: "BEAR!",
+    hp: 30,
+    strength: 30,
+    dexterity: 11,
+    adjDx: 11,
+    weapon: "Bear Claw",
+    wDmg: "2d6+2",
+    armor: "Hide",
+    shield: "",
+    armorAbsorbs: 2,
+    adjMovement: 8
+  },
+  {
+    id: 4,
+    name: "SNEK!",
+    hp: 8,
+    strength: 8,
+    dexterity: 12,
+    adjDx: 12,
+    weapon: "Fangs",
+    wDmg: "1d6+1",
+    armor: "",
+    shield: "",
+    armorAbsorbs: 0,
+    adjMovement: 6
+  }
+];
