ec51e7a094454b6e3ec3f2de69ab366c9a414002
ivancho277 <rouskoi@live.com>
2019-05-12 18:33:52 -0700

    back to how it was

--- client/src/components/CombatSection/index.js
+++ client/src/components/CombatSection/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import DiceComponent from "../DiceRoller/";
 import "./style.css";
-import { elements } from "../constants/index";
-
+import { element } from "prop-types";
+import Defender from "../Defender"
 let numDice = 3;
 function CombatSection(props) {
   //{ elements } = this.props.elements
@@ -38,74 +38,25 @@ function CombatSection(props) {
           </div>
           <DiceComponent numDice={numDice} />
         </div>
-        {/* <div className="col-2" idName="attacker-section">
-          <div className="card box-shadow" idName="attacker-card">
-            <span className="defender-title">Defender Option 1</span>
-            <div className="card-header text-center font-weight-bold">Wuf</div>
-            <div idName="attacker-stats" style={{ textAlign: "left" }}>
-              key=key <br />
-              HP: 11
-              <br />
-              STR: 11 <br />
-              DX: 13 <br />
-              adjDX: 10 <br />
-              Weapon: Short Sword <br />
-              wDmg: 2d6-1 <br />
-              Armor: Leather Armor
-              <br />
-              Shield: Large Shield <br />
-              armorAbsorbs(hp): 4 <br />
-              adjMovement: 8<br /> <br />
-              <button variant="danger">ATTACK WUF! </button>
-              <DiceComponent numDice={numDice} />
-            </div>
-          </div>
-        </div>
-        <div className="col-2" idName="attacker-section">
-          <div className="card box-shadow" idName="attacker-card">
-            <span className="defender-title">Defender Option 2</span>
-            <div className="card-header text-center font-weight-bold">Bear</div>
-            <div idName="attackerStats" style={{ textAlign: "left" }}>
-              key=key <br />
-              HP: 11
-              <br />
-              STR: 11 <br />
-              DX: 13 <br />
-              adjDX: 10 <br />
-              Weapon: Short Sword <br />
-              wDmg: 2d6-1 <br />
-              Armor: Leather Armor
-              <br />
-              Shield: Large Shield <br />
-              armorAbsorbs(hp): 4 <br />
-              adjMovement: 8<br /> <br />
-              <button variant="danger">ATTACK BEAR! </button>
-              <DiceComponent numDice={numDice} />
-            </div>
-          </div>
-        </div>
-        <div className="col-2" idName="attacker-section">
-          <div className="card box-shadow" idName="attacker-card">
-            <span className="defender-title">Defender Option 3</span>
-            <div className="card-header text-center font-weight-bold">Snek</div>
-            <div idName="attackerStats" style={{ textAlign: "left" }}>
-              key=key <br />
-              HP: 11
-              <br />
-              STR: 11 <br />
-              DX: 13 <br />
-              adjDX: 10 <br />
-              Weapon: Short Sword <br />
-              wDmg: 2d6-1 <br />
-              Armor: Leather Armor
-              <br />
-              Shield: Large Shield <br />
-              armorAbsorbs(hp): 4 <br />
-              adjMovement: 8<br /> <br />
-              <button variant="danger">ATTACK SNEK! </button>
-              <DiceComponent numDice={numDice} />
-            </div>
-          // </div> */}
+        {props.elements.map((defender, i) => {
+            if (i === 0) {
+              return;
+            } else {
+              return (
+                <Defender
+                  key={i}
+                  name={defender.name}
+                  strength={defender.strength}
+                  dexterity={defender.dexterity}
+                  adjDx={defender.adjDx}
+                  weapon={defender.weapon}
+                  wDmg={defender.wDmg}
+                />
+              );
+            }
+          })}
+
+
       </div>
     </div>
   );
--- client/src/components/Defender/index.js
+++ client/src/components/Defender/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import DiceComponent from "../DiceRoller/";
-// import "./style.css";
+import "./style.css";
 import { element } from "prop-types";
 
 let numDice = 3;
 function Defender(props) {
   return (
-    <div className="col-2 defender-section">
+    <div className="col-2" idName="attacker-section">
       <div className="card box-shadow" idName="attacker-card">
         <span className="defender-title">{props.name}</span>
         <div className="card-header text-center font-weight-bold">Wuf</div>
         <div idName="attacker-stats" style={{ textAlign: "left" }}>
-          key=key <br />
+          key={props.key} <br />
           HP: {props.hp}
           <br />
           STR: {props.strength} <br />
@@ -20,12 +20,12 @@ function Defender(props) {
           Weapon: {props.weapon}
           <br />
           wDmg: {props.wDmg} <br />
-          Armor: Leather Armor
+          Armor: {props.armor}
           <br />
-          Shield: Large Shield <br />
-          armorAbsorbs(hp): 4 <br />
-          adjMovement: 8<br /> <br />
-          <button variant="danger">ATTACK WUF! </button>
+          Shield: {props.shield} <br />
+          armorAbsorbs(hp): {props.armorAbsorbs} <br />
+          adjMovement: {props.adjMovement}<br /> <br />
+          
           <DiceComponent numDice={numDice} />
         </div>
       </div>
