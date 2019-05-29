087e88874f43d895170737290755f19d36cb3ff2
Jordan Babbitt <JBabbitt27@gmail.com>
2019-05-29 14:40:26 -0700

    fixed buttons

---
+++ client/src/components/CharacterSelect/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function CharacterSelect() {
+  return (
+    <div>
+      <button
+        id="instBox"
+        type="button"
+        class="btn btn-primary"
+        href="/characterSelect"
+        >
+          Start Game
+
+      </button>
+
+
+            </div>
+
+  );
+}
+
+export default CharacterSelect;
